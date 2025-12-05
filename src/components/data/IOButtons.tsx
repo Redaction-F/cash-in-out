67cee3bb92ce7298888db4f572d9ca3ad2c4e1b9
Redaction-F <koebijakobiteorhang@gmail.com>
2025-12-05 11:22:30 +0900

    #17

---
+++ src/components/data/IOButtons.tsx
@@ -0,0 +1,41 @@
+import { invoke } from "@tauri-apps/api";
+import { open } from "@tauri-apps/api/dialog";
+
+function IOButtions() {
+  async function outputFile() {
+    await invoke<void>("write_in_csv", {}).then(() => {
+      alert("ファイルに出力しました。");
+    }, (e) => {
+      console.log(e);
+      alert("エラーが発生しました。エラーメッセージは以下の通りです\n" + String(e));
+    });
+  }
+  async function inputFile() {
+    let selected: string | string[] | null = await open({
+      multiple: false,
+      directory: false, 
+      filters: [{
+        name: 'Text',
+        extensions: ['csv']
+      }]
+    });
+    if (selected === null || Array.isArray(selected)) {
+      return;
+    }
+    await invoke<void>("read_from_csv", {fileName: selected}).then(() => {
+      alert("ファイルから入力しました。");
+    }, (e) => {
+      console.log(e);
+      alert("エラーが発生しました。エラーメッセージは以下の通りです\n" + String(e));
+    });
+  }
+
+  return (
+    <div className="io-container">
+      <button type="button" className="io-button" onClick={outputFile}>ファイル出力</button>
+      <button type="button" className="io-button" onClick={inputFile}>ファイル入力</button>
+    </div>
+  )
+}
+
+export default IOButtions;
